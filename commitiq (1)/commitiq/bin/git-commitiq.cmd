e2a3f56061cd6b0ad47850daa20143c505bd13d7
Masupalli Kushwanth <165185770+kushwanth-masupalli@users.noreply.github.com>
2026-08-14 11:32:37 +0530

    Delete commitiq (1) directory

--- commitiq (1)/commitiq/bin/git-commitiq.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-where bash >nul 2>nul
-if errorlevel 1 (
-  echo commitiq: bash.exe not found on PATH - install Git for Windows ^(which bundles it^) and make sure it's on PATH. 1>&2
-  exit /b 1
-)
-bash "%~dp0git-commitiq" %*
